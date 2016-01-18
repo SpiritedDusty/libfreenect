63dcf3962cd6e716a38bac7585b358e5564e4ea1
SpiritedDusty <spiriteddusty@gmail.com>
2016-01-17 23:17:35 -0800

    Added freenect_sync_get_ir_cv

--- wrappers/opencv/libfreenect_cv.c
+++ wrappers/opencv/libfreenect_cv.c
@@ -25,3 +25,17 @@ IplImage *freenect_sync_get_rgb_cv(int index)
 	cvSetData(image, data, 640*3);
 	return image;
 }
+
+IplImage *freenect_sync_get_ir_cv(int index)
+{
+	static IplImage *image = 0;
+	static char *data = 0;
+	if (!image)
+		image = cvCreateImage(cvSize(640, 480), 8, 1);
+	unsigned int timestamp;
+	if (freenect_sync_get_video
+		((void **)&data, &timestamp, index, FREENECT_VIDEO_IR_8BIT))
+		return NULL;
+	cvSetData(image, data, 640);
+	return image;
+}
--- wrappers/opencv/libfreenect_cv.h
+++ wrappers/opencv/libfreenect_cv.h
@@ -8,6 +8,7 @@ extern "C" {
 
 	IplImage *freenect_sync_get_depth_cv(int index);
 	IplImage *freenect_sync_get_rgb_cv(int index);
+	IplImage *freenect_sync_get_ir_cv(int index);
 
 #ifdef __cplusplus
 }
